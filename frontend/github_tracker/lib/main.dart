515fa18266528ef39dbac96b1e2f07db6ec8c027
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-03-11 02:51:39 +0000

    Add frontend (#39)
    * Add frontend
    
    * Fix move

---
+++ frontend/github_tracker/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'GHT',
+    home: Scaffold(
+      body: const Center(
+        child: Text(
+          'WIP',
+          style: TextStyle(
+            fontSize: 64,
+          ),
+        ),
+      ),
+    ),
+  ));
+}
---
+++ frontend/github_tracker/test/widget_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:github_tracker/main.dart';
+
+void main() {
+  // todo
+}
