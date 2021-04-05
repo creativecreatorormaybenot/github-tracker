56fd9d054403c57d345788c6a0b71f4e8dad3582
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-04-05 23:42:30 +0000

    Organize imports

--- frontend/github_tracker/lib/models/repo_stats.dart
+++ frontend/github_tracker/lib/models/repo_stats.dart
@@ -1,9 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'repo_stats.freezed.dart';
-
 part 'repo_stats.g.dart';
 
 /// Model for stats of a single repo.
