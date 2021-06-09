9a8c8ceb9ea55772b1f07125e36140f2e7e55fde
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-06-09 00:13:31 +0000

    Finalize backup setup (#146)
    * Fix admin client (I know the branch name has a typo)
    
    * Fix admin client usage
    
    * Finalize setup

--- backend/functions/src/backups.ts
+++ backend/functions/src/backups.ts
@@ -1,24 +1,18 @@
 import * as functions from 'firebase-functions'
-import * as firestore from '@google-cloud/firestore'
+import * as admin from 'firebase-admin'
 
+const client = new admin.firestore.v1.FirestoreAdminClient()
 const bucket = 'gs://github-tracker-backups'
 // The repos collection is the only collection that contains
 // meaningful data for backups. The stats collection is completely
 // ephemeral as the data is updated every 15 minutes.
-// const collections = ['repos']
-// Export all collections for now for two reason:
-// 1. Cannot forget to add new collections to the backups :)
-// 2. It should be "fun" to see a snapshot of the stats collection
-//    in backups and it basically contains no data (size-wise)
-//    anyway, which is why we are fine.
-const collections: string[] = []
+const collections = ['repos']
 
 export const backup = functions.pubsub
   .schedule('0 0 */15 * *')
   .onRun(async (context) => {
     const projectId =
       process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT
-    const client = new firestore.v1.FirestoreAdminClient()
     const databaseName = client.databasePath(
       projectId!,
       '(default)'
@@ -37,7 +31,7 @@ export const backup = functions.pubsub
         ? 'all collections'
         : collections.join(', ')
     functions.logger.info(
-      `Exporting everything from ${collectionNames} from Firestore ` +
+      `Exporting everything from ${collectionNames} in ${databaseName} to ${bucket}` +
         `with the operation name "${response['name']}".`
     )
   })
