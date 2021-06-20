07468b90a3b769d51452de7ca6ff0111e88a4058
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-06-20 23:34:08 +0000

    Refactor stats table

---
+++ frontend/github_tracker/lib/widgets/repo_position.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:github_tracker/models/repo_stats.dart';
+
+/// Widget that displays the position information for a single repo in a
+/// horizontal layout.
+///
+/// This is usually inserted into a stats table cell.
+class RepoPosition extends StatelessWidget {
+  /// Creates a [RepoPosition] widget.
+  const RepoPosition({Key? key, required this.stats}) : super(key: key);
+
+  /// The stats data for this widget.
+  final RepoStats stats;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          stats.latest.position.toString(),
+        ),
+      ],
+    );
+  }
+}
