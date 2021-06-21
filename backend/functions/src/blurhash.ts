b74d1722cfadc46f2ac20079cd19a1c212b6ecbb
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-06-21 01:08:19 +0000

    Use sharp for encoding blur hash (#161)

--- backend/functions/src/blurhash.ts
+++ backend/functions/src/blurhash.ts
@@ -1,40 +1,21 @@
+import sharp = require('sharp')
 import { encode } from 'blurhash'
 
 export async function blurhashFromImage(
   imageUrl: string
 ): Promise<string> {
-  const image = await loadImage(imageUrl)
-  const imageData = getImageData(image)
-  return encode(
-    imageData.data,
-    imageData.width,
-    imageData.height,
+  const result = await sharp(imageUrl)
+    .raw()
+    .ensureAlpha()
+    .resize(32, 32, { fit: 'inside' })
+    .toBuffer({ resolveWithObject: true })
+
+  const hash = encode(
+    new Uint8ClampedArray(result.data),
+    result.info.width,
+    result.info.height,
     4,
     4
   )
-}
-
-async function loadImage(
-  imageUrl: string
-): Promise<HTMLImageElement> {
-  return new Promise((resolve, reject) => {
-    const img = new Image()
-    img.onload = () => resolve(img)
-    img.onerror = (...args) => reject(args)
-    img.src = imageUrl
-  })
-}
-function getImageData(image: HTMLImageElement): ImageData {
-  const canvas = document.createElement('canvas')
-  canvas.width = image.width
-  canvas.height = image.height
-
-  const context = canvas.getContext('2d')!
-  context.drawImage(image, 0, 0)
-  return context.getImageData(
-    0,
-    0,
-    image.width,
-    image.height
-  )
+  return hash
 }
