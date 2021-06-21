df13081521a86f6566354e325f0fd47569acaa00
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-06-21 03:24:21 +0000

    Further improve auth UX (#170)

--- frontend/github_tracker/lib/providers/auth.dart
+++ frontend/github_tracker/lib/providers/auth.dart
@@ -3,4 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_tracker/data/auth.dart';
 
 /// Provider that signs in and then provides the signed in user.
-final signedInUser = FutureProvider<User>((ref) => signIn());
+final signedInUser = Provider<User?>((ref) {
+  final user = ref.watch(_futureUser);
+  final data = user.data;
+
+  // Try to return the user synchronously if possible.
+  if (data == null) return currentUser;
+  return data.value;
+});
+
+final _futureUser = FutureProvider<User>((ref) => signIn());
