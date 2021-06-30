292f90f7122b7a63dfc201329c1e7a452330ac62
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-06-30 03:24:15 +0000

    Fix session barrier (#194)
    * prod debug auth barrier
    
    * fix
    
    * final fix

--- frontend/github_tracker/lib/providers/auth.dart
+++ frontend/github_tracker/lib/providers/auth.dart
@@ -3,13 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_tracker/data/auth.dart';
 
 /// Provider that signs in and then provides the signed in user.
-final signedInUser = Provider<User?>((ref) {
-  final user = ref.watch(_streamedUser);
-  final data = user.data;
-
-  // Try to return the user synchronously if possible.
-  if (data == null) return currentUser;
-  return data.value;
+///
+/// This will not update when the user is logged out (does not report `null`
+/// values).
+final signedInUser = StreamProvider<User>((ref) {
+  return signIn().where((event) => event != null).cast();
 });
-
-final _streamedUser = StreamProvider<User>((ref) => signIn());
