033c7ddf76721d47faf0c021004727b6264e1048
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-04-23 22:22:32 +0000

    Fix analysis

--- frontend/github_tracker/lib/main.dart
+++ frontend/github_tracker/lib/main.dart
@@ -7,8 +7,13 @@ import 'package:url_strategy/url_strategy.dart';
 void main() {
   setPathUrlStrategy();
   runApp(const App());
-  SchedulerBinding.instance!.addPostFrameCallback((_) {
+  _binding!.addPostFrameCallback((_) {
     // Only remove the loader after the initial frame.
     removeWebLoader();
   });
 }
+
+// Temporary workaround for making analysis pass both on stable and beta/master
+// as SchedulerBinding.instance is not nullable on beta/master.
+// See https://github.com/creativecreatorormaybenot/github-tracker/runs/6142829858?check_suite_focus=true.
+SchedulerBinding? get _binding => SchedulerBinding.instance;
