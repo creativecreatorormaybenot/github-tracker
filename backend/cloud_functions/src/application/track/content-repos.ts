a2a66ae1ff723d884b8ededc754cff177cded023
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-11-30 17:43:16 +0000

    Untrack several content repos (#571)
    * Untrack several content repos
    
    * Add TheAlgorithms/Java

--- backend/cloud_functions/src/application/track/content-repos.ts
+++ backend/cloud_functions/src/application/track/content-repos.ts
@@ -97,4 +97,8 @@ export const contentRepos: Array<string> = [
   'sdmg15/Best-websites-a-programmer-should-visit',
   'practical-tutorials/project-based-learning',
   'GrowingGit/GitHub-Chinese-Top-Charts',
+  'microsoft/Web-Dev-For-Beginners',
+  'lydiahallie/javascript-questions',
+  'kelseyhightower/nocode',
+  'TheAlgorithms/Java',
 ];
